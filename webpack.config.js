d94af4b46bf124cc08ec576c67b09493e371a2c5
ColetteThomson <colette_f_thomson@yahoo.co.uk>
2023-10-24 13:37:06 +0000

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const path = require('path')
+// entry and output files for the project & define loaders (tells webpack what to do with entry file/s)
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve('dist'),
+        filename: 'main.js'
+    },
+    module: {
+        loaders: [
+            { test: /.jsx?$/, loader: 'babel-loader', exclude: /node_modules/
+            }
+        ]
+    }
+}
